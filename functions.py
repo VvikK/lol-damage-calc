15d3409dc432d572ef3ce2d5c3eee96a1d373cb6
VvikK <78823821+VvikK@users.noreply.github.com>
2024-05-02 06:45:34 -0400

    very basic general case

--- functions.py
+++ functions.py
@@ -1,9 +1,22 @@
 from classes import *
 
-def damage(champ1, ability):
+#damage of one ability
+def damage(champ1, champ2, ability):
     damage = 0
     damage_m = champ1.abilities[ability].magic_damage + (ability.ap_scaling * champ1.base_stats.ap) 
     damage_p = champ1.abilities[ability].physical_damage + (ability.ad_scaling * champ1.base_stats.ad)
     damage_t = ability.true_damage
 
+    armor = champ2.base_stats.armor*(1-champ1.base_stats.armor_pen) - champ1.base_stats.armor_pen_flat
+    mr = champ2.base_stats.mr*(1-champ1.base_stats.mr_pen) - champ1.base_stats.mr_pen_flat
+
+    damage = damage_m*(100/(100+armor)) + damage_p*(100/(100+mr)) + damage_t
+    return damage
+
+#damage of one aa, not accounting for crit
+def auto(champ1, champ2):
+    damage = 0
+    damage_p = champ1.base_stats.ad
+    armor = champ2.base_stats.armor*(1-champ1.base_stats.armor_pen) - champ1.base_stats.armor_pen_flat
+    damage = damage_p*(100/(100+armor))
     return damage
